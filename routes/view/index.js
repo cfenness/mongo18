9b867562a8aa27d570409b7a77e2fc03eacd1b22
Christopher Fennessey <christopherfennessey@Christophers-MacBook-Pro.local>
2018-03-30 20:51:47 -0400

    initial

--- routes/view/index.js
+++ routes/view/index.js
@@ -1,11 +1,9 @@
 var router = require("express").Router();
 
-// This route renders the homepage
 router.get("/", function(req, res) {
   res.render("home");
 });
 
-// This route renders the saved handledbars page
 router.get("/saved", function(req, res) {
   res.render("saved");
 });
